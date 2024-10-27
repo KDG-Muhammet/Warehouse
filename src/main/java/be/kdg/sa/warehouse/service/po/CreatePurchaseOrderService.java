3692c930bb7f2c4641e0f4197eb46b04ceef69fb
muhammet murat <muhammet.murat@student.kdg.be>
2024-10-27 16:18:18 +0100

    Warehouse - limit po to 150kt

--- src/main/java/be/kdg/sa/warehouse/service/po/CreatePurchaseOrderService.java
+++ src/main/java/be/kdg/sa/warehouse/service/po/CreatePurchaseOrderService.java
@@ -1,5 +1,6 @@
 package be.kdg.sa.warehouse.service.po;
 
+import be.kdg.sa.warehouse.controller.dto.po.OrderLineDto;
 import be.kdg.sa.warehouse.controller.dto.po.PurchaseOrderDto;
 import be.kdg.sa.warehouse.domain.Buyer;
 import be.kdg.sa.warehouse.domain.Material;
@@ -12,8 +13,11 @@ import be.kdg.sa.warehouse.service.seller.SellerService;
 import be.kdg.sa.warehouse.service.material.MaterialService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 @Service
@@ -23,6 +27,8 @@ public class CreatePurchaseOrderService {
     private final SellerService sellerService;
     private final MaterialService materialService;
     private final PurchaseOrderService purchaseOrderService;
+    @Value("${maxAmountPo}")
+    private BigDecimal maxAmountPo;
     private static final Logger logger = LoggerFactory.getLogger(CreatePurchaseOrderService.class);
 
     public CreatePurchaseOrderService(BuyerService buyerService, SellerService sellerService, MaterialService materialService, PurchaseOrderService purchaseOrderService) {
@@ -33,21 +39,31 @@ public class CreatePurchaseOrderService {
     }
     @Transactional
     public void createPurchaseOrder(PurchaseOrderDto purchaseOrderDto) {
-        Buyer buyer = buyerService.findBuyerByNameAndAddress(purchaseOrderDto.getBuyerName(), purchaseOrderDto.getBuyerAddress());
-        Seller seller = sellerService.findSellerByNameAndAddress(purchaseOrderDto.getSellerName(), purchaseOrderDto.getSellerAddress());
-        // Maak de nieuwe PurchaseOrder aan
-        PurchaseOrder purchaseOrder = new PurchaseOrder(purchaseOrderDto.getPoNumber(), purchaseOrderDto.getDate(), seller, buyer, new ArrayList<>());
-        purchaseOrder.setStatus(Status.ONGOING);
-        // Stel voor elke OrderLine de PurchaseOrder in
-        purchaseOrderDto.getOrderLines().forEach(orderLineDto -> {
-            logger.info("incoming material:" + orderLineDto.getAmount() + orderLineDto.getMaterialName());
-            Material material = materialService.findMaterialByType(orderLineDto.getMaterialName());
+        BigDecimal totalAmount = BigDecimal.ZERO;
 
-            OrderLine orderLine = new OrderLine(orderLineDto.getAmount(), material);
-            orderLine.setPurchaseOrder(purchaseOrder);
+        for (OrderLineDto lineDto : purchaseOrderDto.getOrderLines()) {
+            totalAmount = totalAmount.add(BigDecimal.valueOf(lineDto.getAmount()));
+        }
 
-            purchaseOrder.getOrderLines().add(orderLine);
-        });
-        purchaseOrderService.create(purchaseOrder);
+        if (totalAmount.compareTo(maxAmountPo) > 0){
+            logger.warn("   you are over the amount limit. Your total amount {}:", totalAmount );
+            throw new IllegalArgumentException("you are going over the limit of " + totalAmount);
+
+        } else {
+            Buyer buyer = buyerService.findBuyerByNameAndAddress(purchaseOrderDto.getBuyerName(), purchaseOrderDto.getBuyerAddress());
+            Seller seller = sellerService.findSellerByNameAndAddress(purchaseOrderDto.getSellerName(), purchaseOrderDto.getSellerAddress());
+            // Maak de nieuwe PurchaseOrder aan
+            PurchaseOrder purchaseOrder = new PurchaseOrder(purchaseOrderDto.getPoNumber(), purchaseOrderDto.getDate(), seller, buyer, new ArrayList<>());
+            purchaseOrder.setStatus(Status.ONGOING);
+            // Stel voor elke OrderLine de PurchaseOrder in
+            purchaseOrderDto.getOrderLines().forEach(orderLineDto -> {
+                logger.info("incoming material:" + orderLineDto.getAmount() + orderLineDto.getMaterialName());
+                Material material = materialService.findMaterialByType(orderLineDto.getMaterialName());
+                OrderLine orderLine = new OrderLine(orderLineDto.getAmount(), material);
+                orderLine.setPurchaseOrder(purchaseOrder);
+                purchaseOrder.getOrderLines().add(orderLine);
+            });
+            purchaseOrderService.create(purchaseOrder);
+        }
     }
 }
